004065b5b8e6101994bc98aa9f9efe0d3985026d
Brandon Bruno <brab@sitecore.net>
2023-10-10 11:36:28 -0400

    Moved summary prompts to code. Added logic to update summary prompt field with value from data.

--- src/sidepanel.js
+++ src/sidepanel.js
@@ -1,21 +1,98 @@
 (function() {
 
-    function init() {
+    let summaryOptions = [
+        {
+            "value": "title",
+            "label": "Title",
+            "prompt": "Create a title for this article"
+        },
+        {
+            "value": "summary-short",
+            "label": "Short Summary (1-sentence)",
+            "prompt": "Create a 1-sentence summary of this article"
+        },
+        {
+            "value": "long-summary",
+            "label": "Long Summary (1 paragraph)",
+            "prompt": "Create a 1-paragraph summary of this article"
+        },
+        {
+            "value": "social-linkedin",
+            "label": "Social: LinkedIn",
+            "prompt": "Write a two sentence summary of this article for a professional business audience"
+        },
+        {
+            "value": "social-facebook",
+            "label": "Social: Facebook",
+            "prompt": "Write a short, fun summary of this article for a Facebook audience"
+        },
+        {
+            "value": "social-x",
+            "label": "Social: X",
+            "prompt": "Write a 1-sentence summary of this article for a Twitter post"
+        },
+        {
+            "value": "meta-keywords",
+            "label": "Meta: Keyword",
+            "prompt": "Create a list of 5 to 10 keywords separated by commas from this article"
+        },
+        {
+            "value": "meta-description",
+            "label": "Meta: Description",
+            "prompt": "Create am SEO-ready 1-sentence description of this article"
+        }
+    ];
 
+    function init () {
+
+        populateSummaryTypeDropdown();
         wireUI();
 
     }
 
-    function wireUI() {
+    function populateSummaryTypeDropdown () {
+
+        let summaryType = document.getElementById("SummaryType");
+
+        // Clear dropdown
+        for (let pos = summaryType.options.length; pos >= 0; pos--) {
+            summaryType.options.remove(pos);
+        }
+
+        // Populate (blank option first, then option list)
+        let option = document.createElement("option");
+        option.value = "";
+        option.text = "";
+        summaryType.add(option);
+
+        summaryOptions.forEach((item) => {
+
+            let option = document.createElement("option");
+            option.value = item.value;
+            option.text = item.label;
+            option.setAttribute("data-prompt", item.prompt);
+
+            summaryType.add(option);
+
+        });
+
+    }
+
+    function wireUI () {
 
         let openAddSummary = document.getElementById("OpenNewSummaryField");
         if (openAddSummary) {
             openAddSummary.addEventListener("click", (e) => openSummaryEditor(e));
         }
 
+        let summaryType = document.getElementById("SummaryType");
+        if (summaryType) {
+            summaryType.addEventListener("change", (e) => handleSummaryTypeChange(e));
+        }
+
     }
 
-    function openSummaryEditor(e) {
+    function openSummaryEditor (e) {
 
         let panel = document.getElementById("NewSummaryField");
         panel.classList.remove("closed");
@@ -23,12 +100,17 @@
 
     }
 
-    function closeSummaryEditor(e) {
+    function closeSummaryEditor (e) {
 
         let panel = document.getElementById("NewSummaryField");
         panel.classList.remove("opened");
         panel.classList.add("closed");
 
+    }
+
+    function handleSummaryTypeChange (e) {
+        var promptText = e.currentTarget.options[e.currentTarget.selectedIndex].getAttribute("data-prompt");
+        document.getElementById("SummaryPrompt").value = promptText;
     }
 
     init();
